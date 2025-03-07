fa5ed8c8327eeba4f6b161d00fc9c9b70c697420
logikmars <dima76673@gmail.com>
2025-03-07 10:47:53 +0200

    work

--- src/pages/FAQ/FAQ.jsx
+++ src/pages/FAQ/FAQ.jsx
@@ -5,7 +5,8 @@ import { useState } from "react";
 const questions = [
     { id: 1, question: "YOUR QUESTION", answer: "Consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." },
     { id: 2, question: "YOUR QUESTION", answer: "Consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." },
-    { id: 3, question: "YOUR QUESTION", answer: "Consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." }
+    { id: 3, question: "YOUR QUESTION", answer: "Consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." },    
+    { id: 4, question: "YOUR QUESTION", answer: "Consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." }
 ];
 
 export default () => {
@@ -25,11 +26,11 @@ export default () => {
          <div className="FAQ__accordion">
             {questions.map((item, index) => (
                 <div key={item.id} className={`FAQ__accordion-item ${openIndex === index ? "open" : ""}`}>
-                    <div className="accordion-header" onClick={() => toggleAccordion(index)}>
-                        <span className='k2_fonts'>{item.question}</span>
-                        <span className="icon">{openIndex === index ? "−" : "+"}</span>
+                    <div className="FAQ__accordion-header" onClick={() => toggleAccordion(index)}>
+                        <span className='k2_fonts FAQ__accordion-header-title'>{item.question}</span>
+                        <span className="FAQ__accordion-header-icon">{openIndex === index ? "−" : "+"}</span>
                     </div>
-                    {openIndex === index && <div className="accordion-content k2_fonts">{item.answer}</div>}
+                    {openIndex === index && <div className="FAQ__accordion-content k2_fonts">{item.answer}</div>}
                 </div>
             ))}
         </div>
